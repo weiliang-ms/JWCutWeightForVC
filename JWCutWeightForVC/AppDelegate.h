ed8fd13fa3cb527cebb7955add4e7c227faf3532
xueZhiXiaWeiLiang <1067599390@qq.com>
2016-04-26 13:01:34 +0800

    Initial Commit

---
+++ JWCutWeightForVC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JWCutWeightForVC
+//
+//  Created by 微凉 on 16/4/26.
+//  Copyright © 2016年 微凉. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JWCutWeightForVC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JWCutWeightForVC
+//
+//  Created by 微凉 on 16/4/26.
+//  Copyright © 2016年 微凉. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
